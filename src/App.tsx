bfb89172a5580b1aa7a6bfce1079dbe2b01d2e5f
Michal Gajda <mgajda36@gmail.com>
2026-03-15 14:53:00 +0100

    Repository setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Attribution from "./components/Attribution";
+
+function App() {
+  return (
+    <>
+      <Attribution />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Attribution.tsx
@@ -0,0 +1,18 @@
+export default function Attribution() {
+  return (
+    <div className="attribution">
+      Challenge by&nbsp;
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        rel="noreferer noopener"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="#" rel="noreferer noopener">
+        Michał Gajda
+      </a>
+      .
+    </div>
+  );
+}
